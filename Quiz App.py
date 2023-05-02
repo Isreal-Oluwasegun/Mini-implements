20d3934a5073ca20dab6af38246fbfcdac0c1c7e
Isreal <120344210+Isreal-Oluwasegun@users.noreply.github.com>
2023-05-02 12:14:08 +0100

    Update Quiz App.py

--- Quiz App.py
+++ Quiz App.py
@@ -1,26 +1,34 @@
 
+# Import all required modules
 from string import ascii_uppercase
 import random
 import json
 question_per_quiz = 7
 
+# Open the file questions.json and save in variable questions
+# The file contains the questions for the quiz app
 with open("Questions.json","r") as json_object:
     QUESTIONS = json.load(json_object)
 
+# 'prepare_questions' functions randomly reorder
+# the questions on every run
 def prepare_questions(questions, num_of_questions):
     num_of_questions = min(num_of_questions, len(questions))
     return random.sample(list(questions.items()), num_of_questions)
 
+# binds alphabet to each alternative options(answers) and get user options
 def get_answer(question, alternatives):
     print(question)
-    labeled_alternative = dict(zip(ascii_uppercase, alternatives))
+    labeled_alternative = dict(zip(ascii_uppercase, alternatives)) # binds ascii_letters(A,B..) to each options then convert to dict
     for label, alternative in labeled_alternative.items():
         print(label, alternative)
     
+    # checks if user's choice(input) is in the available options
     while (choice := input("\nChoice? ").upper()) not in labeled_alternative:
         print(f"Please answer one of {', '.join(labeled_alternative)}")
     return labeled_alternative[choice]
 
+# displays the questions and compare if user's choice is corrcet
 def ask_question(question, alternatives):
     correct_answer = alternatives[0]
     ordered_alternative = random.sample(alternatives, len(alternatives))
